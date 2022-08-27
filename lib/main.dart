473061ec6edd251e3f762ba236072f56e969cf2a
jmapps <jmappsmuslim@gmail.com>
2022-08-27 07:31:42 +0300

    Fixes and added firebase CRUD

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:fire_base/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const RootPage(),
   );
@@ -30,6 +37,10 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final TextEditingController controller = TextEditingController();
+  final databaseReference = FirebaseDatabase.instance.ref();
+  String readText = 'Не получено никаких данных';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +49,81 @@ class _MainPageState extends State<MainPage> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: const Center(
-        child: Text('FireBase test app'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+            MaterialButton(
+              child: const Text('Создать запись'),
+              onPressed: () {
+                createRecord();
+              },
+            ),
+            MaterialButton(
+              child: const Text('Просмотреть запись'),
+              onPressed: () {
+                setState(() {
+                  getData();
+                });
+              },
+            ),
+            MaterialButton(
+              child: const Text('Редактировать запись'),
+              onPressed: () {
+                updateData();
+              },
+            ),
+            MaterialButton(
+              child: const Text('Удалить запись'),
+              onPressed: () {
+                deleteData();
+              },
+            ),
+            Text(
+              readText,
+              style: const TextStyle(
+                fontSize: 25,
+                color: Colors.purple,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
 
+  createRecord() {
+    databaseReference.child("post_1").set(
+      {'title': controller.text},
+    );
+  }
+
+  getData() {
+    databaseReference.child('post_1').once().then(
+          (value) => {
+            readText = value.snapshot.value.toString(),
+          },
+        );
+  }
+
+  updateData() {
+    databaseReference.child('post_1').update(
+      {'title': 'newText'},
+    );
+  }
+
+  deleteData() {
+    databaseReference.child('post_1').remove();
+  }
+}
